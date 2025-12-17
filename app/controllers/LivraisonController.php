64cd06e8accc8e6ffc259d8f874da14a7f02d92b
Azoe007 <hamidullahaymanzara@gmail.com>
2025-12-17 13:36:13 +0300

    livraison

--- app/controllers/LivraisonController.php
+++ app/controllers/LivraisonController.php
@@ -16,14 +16,14 @@ class LivraisonController {
 		$this->app = $app;
 	}
 
-    public function afficherLivraisons() {
+    /*public function afficherLivraisons() {
         $livraisonModel = new LivraisonModel($this->app->db());
         $livraisons = $livraisonModel->getLivraisonS();
 
         $this->app->render('accueil.php', [
             'data' => $livraisons
         ]);
-    }
+    }*/
 
     public function afficherFormulaireAjout() {
         $colisModel = new ColisModel($this->app->db());
@@ -40,6 +40,26 @@ class LivraisonController {
             'livreurs'  => $livreurs,
             'vehicules' => $vehicules
         ]);
+    }
+
+    public function insererLivraison() {
+        $idColis = $this->app->request()->data->idColis;
+        $adresse_destination = $this->app->request()->data->adresse_destination;
+        $date_livraison = $this->app->request()->data->date_livraison;
+        $idVehicule = $this->app->request()->data->idVehicule;
+        $idChauffeur = $this->app->request()->data->idChauffeur;
+        $coutVehicule = $this->app->request()->data->cout_vehicule;
+        $addresse_depart = "Entrepot Central"; // Adresse fixe pour le dépôt
+
+        if (empty($idColis) || empty($adresse_destination) || empty($date_livraison) || empty($idVehicule) || empty($idChauffeur)) {
+            $this->app->redirect('/livraison/form');
+            return;
+        }
+
+        $livraisonModel = new LivraisonModel($this->app->db());
+        $livraisonModel->insertLivraison($idColis, $addresse_depart, $adresse_destination, $date_livraison, $idVehicule, $idChauffeur, $coutVehicule);
+
+        $this->app->redirect('/');
     }
 
 
