6896d063116b31540448ffeef2ca091d8c73aef1
Azoe007 <hamidullahaymanzara@gmail.com>
2025-12-17 10:44:19 +0300

    config

--- app/config/config.php
+++ app/config/config.php
@@ -68,7 +68,7 @@ return [
 	'database' => [
 		// MySQL Example:
 		'host'     => 'localhost:3306',      //Database host (e.g., 'localhost', 'db.example.com')
-		'dbname'   => 'exam_colis_express',   //Database name (e.g., 'flightphp')
+		'dbname'   => 'entreprise_livraison',   //Database name (e.g., 'flightphp')
 		'user'     => 'root',  //Database user (e.g., 'root')
 		'password' => '',  //Database password (never commit real passwords)
 
